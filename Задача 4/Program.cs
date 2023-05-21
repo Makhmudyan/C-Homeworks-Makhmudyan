e8a015c4839d226edcd47dca5b382ce4622cc21f
Marianna <makhmudyan@yandex.ru>
2023-05-21 16:17:22 +0400

    Задача 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int N2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int N3 = int.Parse(Console.ReadLine());
+var numbers = new List<int> { N1, N2, N3};
+int size = 3;
+int index = 0;
+int max = numbers[0];
+while(index < size)
+{
+   if(numbers[index] > max)
+   max = numbers[index];
+   else
+   index++;
+}
+//int max1 = Convert.ToInt64(max); как сделать так, чтобы принимал огромные числа??
+Console.WriteLine($"Максимальное число: {max}.");
+
+
+
+
+
+
+
+
+
+
