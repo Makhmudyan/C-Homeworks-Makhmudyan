a191816b28d80ae3742137dba525c464ea590545
Marianna <makhmudyan@yandex.ru>
2023-05-25 13:12:28 +0400

    Dir Homework 1 created

---
+++ C# Homework 1 Makhmudyan/Задача 4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+long N1 = long.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+long N2 = long.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+long N3 = long.Parse(Console.ReadLine());
+var numbers = new List<long> { N1, N2, N3};
+long size = 3;
+int index = 0;
+long max = numbers[0];
+while(index < size)
+{
+   if(numbers[index] > max)
+   max = numbers[index];
+   else
+   index++;
+}
+//int max1 = Convert.ToInt64(max); как сделать так, чтобы принимал огромные числа??
+Console.WriteLine($"Максимальное число: {max}.");
+
+
+
+
+
+
+
+
+
+
---
+++ C# Homework 1 Makhmudyan/Задача 6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+while(number % 2 != 0)
+
+{
+    Console.WriteLine("Данное число является нечетным. Введите другое число: ");
+    number = int.Parse(Console.ReadLine());
+}
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} является четным.");
+}
+
+// Сделала таким образом, чтобы усложнить себе задачу
