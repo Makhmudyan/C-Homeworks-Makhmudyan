24357f0731a9a3deba6596ae9e817ec02cb2125d
Marianna <makhmudyan@yandex.ru>
2023-05-25 12:45:48 +0400

    Задача 14

---
+++ Задача 14/Program.cs
@@ -0,0 +1,15 @@
+﻿// // Напишите программу, которая принимает на вход число и проверяет,
+//  кратно ли оно одновременно 7 и 23.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else 
+{
+     Console.WriteLine($"Число {number} не кратно 7 и 23");
+}
