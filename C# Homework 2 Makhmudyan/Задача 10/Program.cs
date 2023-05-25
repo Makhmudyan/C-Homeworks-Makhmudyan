36ed5d0c03eed6880af1f9176cb56d0524e24823
Marianna <makhmudyan@yandex.ru>
2023-05-25 21:20:55 +0400

    Updated 2

--- C# Homework 2 Makhmudyan/Задача 10/Program.cs
+++ C# Homework 2 Makhmudyan/Задача 10/Program.cs
@@ -10,11 +10,11 @@ Console.WriteLine("Введите трехзначное число: ");
 int number = int.Parse(Console.ReadLine());
 
 
-while (number < 100 || number > 999) 
+while (Math.Abs(number) < 100 || Math.Abs(number) > 999) 
 {
     Console.WriteLine("Введено не трехзначное число: ");
     Console.Write("Введите трехзначное число: ");
     number = int.Parse (Console.ReadLine());
 }
-    int result = (number / 10) % 10;
+    int result = Math.Abs((number) / 10) % 10;
     Console.WriteLine($"{result}");
