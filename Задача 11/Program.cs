9acf39622879c86006ea5fb73ba36fde712fc40e
Marianna <makhmudyan@yandex.ru>
2023-05-25 12:45:06 +0400

    Задача 11

---
+++ Задача 11/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая вводит случайное трехзначное число 
+//и удаляет вторую цифру этого числа.
+
+Console.Clear();
+int num = new Random().Next(100,1000);
+
+Console.WriteLine(num);
+int result = (num / 100) * 10 + num % 10;
+
+Console.WriteLine($"{result}");
+
