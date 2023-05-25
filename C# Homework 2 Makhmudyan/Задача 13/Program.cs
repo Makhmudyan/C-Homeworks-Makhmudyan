7b7251f7a3162955aed8acb679135588ab6d5afe
Marianna <makhmudyan@yandex.ru>
2023-05-25 18:51:01 +0400

    Dir Homework 2 Makhmudyan created

---
+++ C# Homework 2 Makhmudyan/Задача 13/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+long number = long.Parse (Console.ReadLine());
+int index = 0;
+
+if (Math.Abs((number) / 100) % 10 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    while (Math.Abs((number) / 1000) != 0)
+    {
+        number = number / 10;
+        index++;
+    }
+}
+Console.WriteLine($"{Math.Abs(number) % 10}");
---
+++ C# Homework 2 Makhmudyan/Задача 15/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+*/
+
+Console.Clear();
+Console.WriteLine("Введите номер дня недели: ");
+int numberDayOfWeek = int.Parse (Console.ReadLine());
+
+while (numberDayOfWeek < 6 || numberDayOfWeek > 7) 
+{
+    Console.WriteLine("Введен неправильный номер");
+    Console.Write("Введите номер дня недели: ");
+    numberDayOfWeek = int.Parse (Console.ReadLine());
+}
+
+Console.WriteLine("Это выходной день");
