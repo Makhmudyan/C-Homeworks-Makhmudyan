0123da0f9ed39fbdad1432e85fe15de27dc55fc5
Marianna <makhmudyan@yandex.ru>
2023-05-25 12:44:12 +0400

    Задача 9

---
+++ Задача 9/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишиту программу, которая вводит случаное число из отрезка [10,99]
+//и показывает наибольшую цифру числа.
+
+Console.Clear();
+int num = new Random().Next(10,100);
+int num1 = num / 10;
+int num2 = num % 10;
+int max = num1;
+
+if (num1 > max)
+{
+     max = num1;
+}
+Console.WriteLine($"Max no of {num} is {max}");
