c3c90512ed8c80a1f005f3d1e2a412382efb074a
Marianna <makhmudyan@yandex.ru>
2023-06-08 16:24:59 +0400

    Homework 6

---
+++ C# Homework 6 Makhmudyan/Задача 41/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 41: 
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 
+
+Console.Clear();
+Console.WriteLine("Введите числа через пробел: ");
+string numbers = Console.ReadLine();
+int [] a = GetArrayFromString(numbers);
+Console.WriteLine($"{String.Join (", ", numbers)} -> {GetPos(a)[0]} ");
+
+int [] GetArrayFromString(string stringArray)
+{
+    string [] nums = stringArray.Split(" ");
+    int [] result = new int [nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        result [i] = int.Parse (nums[i]);
+    }
+    return result;
+}
+
+int [] GetPos (int [] arr)
+{//int count = 0;
+    int [] result = new int [1]; //for(int i = 0; i < arr.Length; i++)
+    //if(arr[i]>=0)
+    {
+    }
+    foreach(int el in arr)
+    {
+        result [0] += el > 0? 1:0;
+    }
+    return result;
+}
---
+++ C# Homework 6 Makhmudyan/Задача 43/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 43: 
+//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+//задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.WriteLine("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine());
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+else
+{
+   double [] a = Getint(b1,k1,b2,k2);
+   Console.WriteLine($"координаты точки пересечения = [{a[0]}:{a[1]}]");
+}
+
+
+double [] Getint (double b1, double k1, double b2, double k2)
+{
+    double [] result = new double [2];
+    result [0] = (b2-b1) / (k1-k2);
+    result [1] = k1 * result[0] + b1;
+    return result;
+}
